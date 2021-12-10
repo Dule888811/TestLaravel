363f382b93cdde6e48d15e299a94b300dd86a10f
Dule888811 <dusan.pavlovic88@yahoo.com>
2021-12-10 10:44:04 +0100

    WIP

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
       //  $this->call(CategoriesTableSeeder::class);
        // $this->call(ProductsTableSeeder::class);
        // $this->call(SubcategoriesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+       // $this->call(UsersTableSeeder::class);
     }
 }

e0f5b5c218399dceb06ece4e82c7f7b4c66de27e
Dule888811 <dusan.pavlovic88@yahoo.com>
2021-12-17 18:17:12 +0100

    fix-css

--- resources/views/admin/main/index.blade.php
+++ resources/views/admin/main/index.blade.php
@@ -1,12 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
+    <nav role="navigation">
+
+        <ul>
+
+            <ul class="categories">Categories
+                @foreach($categories as $category)
+                    <ul class="Subc hide">
+                        @if($category->Subcategories()->count() > 0)
+                            <br>
+                            <h5 class="hide">Category Subcategiries{{$category->name}}</h5>:
+                            <br>
+                            @foreach($category->Subcategories()->get() as $subcategory)
+
+                                @if($subcategory['category_id'] == $category['id'])
+                                    <li class="hide">Subcategory:{{$subcategory['name']}}</li>
+                                @else
+                                    @continue;
+                                @endif
+                                <hr class="rounded">
+                            @endforeach
+                        @endif
+                        @if($category->Products()->count() > 0)
+                            @foreach($category->Products()->get() as $product)
+                                @if($product['category_id'] == $category['id'])
+                                    <ul class="categories-products-ul">Category Products:{{$category->name}}
+                                        <li class="cat-products"><h5>Title:{{$product['title']}}</h5></li>
+                                        <li class="cat-products">Name of use:{{$product->user['name']}}</li>
+                                        <li class="cat-products">location:{{$product->user['location']}}</li>
+                                        <li class="cat-products">phone:{{$product->user['phone']}}</li>
+                                        <li class="cat-products">price:{{$product['price']}}</li>
+                                        <li class="cat-products">1=new:{{$product['new']}}</li>
+                                        <li class="cat-products">created at{{$product['created_at']}}</li>
+
+                                        <a href="/product">Go to ads</a>
+                                        <br>
+                                        <hr class="rounded">
+                                    </ul>
+                                @else
+                                    @continue;
+                                @endif
+                            @endforeach
+
+
+                        @endif
+
+
+                    </ul>
+
+                @endforeach
+
+
+            </ul>
+
+
+        </ul>
+
+    <ul>
+
+       
+    </nav>
     <div class="container text-center">
 
         @foreach ($products as $product)
             <div class="form-input">
                 <label for="destripton">destripton:
-                    <input type="text"  name="Productdescription" value={{$product->description}}></label>
+                    <input type="text" name="Productdescription" value={{$product->description}}></label>
             </div>
             <div class="form-input">
                 <label for="title">Title:
